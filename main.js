6a890ccd49ebef1511e92198cd9696ebf4dade36
Breno Vasc <beniolimavasc@gmail.com>
2024-02-06 22:06:26 -0300

    Piano JavaScript

---
+++ main.js
@@ -0,0 +1,51 @@
+// get all keys
+const keys = document.querySelectorAll(".key")
+
+function playNote(event){
+  // keyCode
+let audioKeyCode = getKeyCode(event);
+  // typed or pressed key
+const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+  // if key exists
+const cantFoundAnyKey = !key;
+if(cantFoundAnyKey){
+return;
+}
+  addPlayingClass(key)
+  playAudio(audioKeyCode)
+};
+function addPlayingClass(){
+  key.classList.add('playing');
+}
+function getKeyCode(event) {
+let keyCode;
+
+const isKeyBoard = event.type === "keydown"; 
+if(KeyboardEvent){
+  keyCode = event.keyCode
+} else {
+  keyCode = event.target.dataset.key
+}
+return keyCode
+};
+
+function playAudio(audioKeyCode){
+const audio = document.querySelector(`audio[data-key${audioKeyCode}]`)
+audio.currentTime = 0;
+audio.play()
+};
+
+function removePlayingClass(event){
+event.target.classList.remove("playing")
+}
+
+function registerEvents (){ 
+  // click with mouse
+keys.forEach( function(key){
+  key.addEventListener("click", playNote)
+  key.addEventListener('transitionend', removePlayingClass)
+});
+// keyboard type
+window.addEventListener("keydown", playNote);
+}
+window.addEventListener("load", registerEvents);
